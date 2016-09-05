e8539fefd9254386c0297158551d2578b58291b2
Sift Labs <10852534+churlongo@users.noreply.github.com>
2016-09-05 13:07:00 -0600

    chore: bump jws

---
+++ src/tokenscope/decode.py
@@ -0,0 +1,13 @@
+"""Decode JWT and JWS compact serialization into header and payload.
+
+The one rule of this module is that it never trusts what it decodes. It splits
+the compact serialization on '.', base64url-decodes the first two segments, and
+parses them as JSON. Everything it finds is data to be audited, not a fact to
+act on. The signature segment is kept as raw text and never used to make a trust
+decision here; verification lives in verify.py and is only attempted when the
+caller supplies a secret.
+
+A JWT has three segments (header.payload.signature). A JWS with detached or
+absent signature, or an unsecured token with `alg: none`, may present an empty
+third segment. Both shapes are decoded and reported honestly.
+"""
