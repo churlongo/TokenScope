4cd01325b2e12cee17d0b4e3e218420c96f6cfaa
Sift Labs <10852534+churlongo@users.noreply.github.com>
2018-01-11 11:27:00 -0600

    style: clean up security

--- src/tokenscope/policy.py
+++ src/tokenscope/policy.py
@@ -5,3 +5,9 @@ whatever a token happens to assert. Keeping the policy in one place means the
 auditor never has to guess what "too long" or "wrong algorithm" means; the
 answer is stated once and applied uniformly.
 
+The default policy below is conservative and is used when the CLI is run without
+a policy file. It is deliberately strict so that a clean result means something.
+"""
+
+from __future__ import annotations
+
