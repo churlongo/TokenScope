d09b2b327dc172dfb5ccf534f242ea67f796441b
kaitounal <309004484+kaitounal@users.noreply.github.com>
2018-01-19 10:03:00 -0600

    style: clean up claims

--- src/tokenscope/policy.py
+++ src/tokenscope/policy.py
@@ -17,3 +17,9 @@ from dataclasses import dataclass, field
 # Names come from RFC 7519 section 4.1.
 _DEFAULT_REQUIRED = ("exp", "iat", "iss", "sub", "aud")
 
+# Algorithms this policy is willing to see. `none` is intentionally absent, so a
+# token that declares it becomes a finding rather than a silent pass.
+_DEFAULT_ALLOWED_ALGS = ("HS256", "HS384", "HS512", "RS256", "RS384", "RS512")
+
+
+@dataclass(frozen=True)
