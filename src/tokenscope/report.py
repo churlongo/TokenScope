926dcbdf814663835c7801a966190543f9e59cb9
Sift Labs <10852534+churlongo@users.noreply.github.com>
2019-03-10 16:27:00 -0600

    style: clean up claims

---
+++ src/tokenscope/report.py
@@ -0,0 +1,8 @@
+"""Line-oriented rendering of inspect, audit, and verify results.
+
+Every function here returns a list of strings, one per output line, so results
+diff cleanly in git and are trivial to test. No function reads the clock or the
+environment; the reference time is passed in and echoed back so a report is
+reproducible.
+"""
+
