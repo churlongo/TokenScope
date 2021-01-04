f50d34a7e0e4d7479d033b891087a530658aeb2c
Sift Labs <10852534+churlongo@users.noreply.github.com>
2021-01-04 09:26:00 -0600

    perf: improve jws

--- tests/test_tokenscope.py
+++ tests/test_tokenscope.py
@@ -30,3 +30,21 @@ def _b64(data: bytes) -> str:
 
 def make_token(header: dict, payload: dict, secret: bytes | None = None) -> str:
     h = _b64(json.dumps(header, sort_keys=True, separators=(",", ":")).encode())
+    p = _b64(json.dumps(payload, sort_keys=True, separators=(",", ":")).encode())
+    signing_input = (h + "." + p).encode("ascii")
+    if secret is None:
+        return h + "." + p + "."
+    sig = hmac.new(secret, signing_input, hashlib.sha256).digest()
+    return h + "." + p + "." + _b64(sig)
+
+
+class B64UrlTests(unittest.TestCase):
+    def test_roundtrip_without_padding(self):
+        encoded = _b64(b"hello world")
+        result = b64url.decode_segment(encoded)
+        self.assertTrue(result.ok)
+        self.assertEqual(result.data, b"hello world")
+
+    def test_padding_added_is_reported(self):
+        # 'hell' is 4 bytes, encoding to 6 base64url chars needing two pad chars.
+        encoded = _b64(b"hell")
